2603fc1627dc44c5be6c0bda3046aa3eabdfb990
Oleg <shlyapin8848@gmail.com>
2024-03-26 16:26:18 +0200

    add pagination for products

--- app_main/products/views.py
+++ app_main/products/views.py
@@ -1,20 +1,26 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_list_or_404, render
 
 from products.models import Products
 
 
-def catalog(request, category_slug):
+def catalog(request, category_slug, page=1):
 
     if category_slug == "all":
         products = Products.objects.all()
     else:
         products = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    paginator = Paginator(object_list=products, per_page=3)
+    current_page = paginator.page(page)
+
     context = {
         "title": "Catalog",
-        "all_prod": products
+        "all_prod": current_page,
+        "slug_url": category_slug
     }
     return render(request=request, template_name="products/catalog.html", context=context)
+
 
 
 def product(request, product_slug):
