7b520d227c1f8bdbd0989627f16439cbc3505c16
Oleg <shlyapin8848@gmail.com>
2024-03-28 13:24:42 +0200

    added connection to postgres

--- app_main/application/settings.py
+++ app_main/application/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'application.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pizza_store',
+        'USER': 'root',
+        'PASSWORD': '111',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
