04a46d6f7da576a2146df39d8be5d0813d2b85bc
Oleg <shlyapin8848@gmail.com>
2024-03-28 12:33:05 +0200

    add Q obj for serching

--- app_main/products/utils.py
+++ app_main/products/utils.py
@@ -5,3 +5,14 @@ from django.db.models import Q
 def query_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    
+    words_for_search = [word for word in query.split() if len(word) >= 3]
+
+    q_object = Q()
+
+    for word in words_for_search:
+        q_object |= Q(description__icontains=word)
+        q_object |= Q(name__icontains=word)
+        print(word)
+
+    return Products.objects.filter(q_object)
