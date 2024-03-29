4006baee3f2f7fee649ea09d5eacca619e514e1d
Oleg <shlyapin8848@gmail.com>
2024-03-29 15:16:08 +0200

    added form for login

---
+++ app_main/users/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "autofocus": True,
+                "class": "form-control",
+                "placeholder": "Введите ваше имя пользователя",
+            }
+        )
+    )
+
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "autocomplete": "current-password",
+                "class": "form-control",
+                "placeholder": "Введите ваш пароль",
+            }
+        ),
+    )
+    error_messages = {
+        "invalid_login": ("Неправильно введено %(username)s или пароль."),
+    }
+
+    class Meta:
+        model = User()
