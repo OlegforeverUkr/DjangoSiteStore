c0e622dc5cfc82901996dbf121ee97089ed5d135
Oleg <shlyapin8848@gmail.com>
2024-04-05 12:42:35 +0300

    added remove from basket

--- app_main/baskets/views.py
+++ app_main/baskets/views.py
@@ -10,7 +10,6 @@ from django.core.exceptions import ObjectDoesNotExist
 
 def basket_add(request):
     product_id = request.POST.get("product_id")
-    print(product_id)
     product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
@@ -44,11 +43,24 @@ def basket_change(request, product_slug):
 
 
 
-def basket_remove(request, basket_id):
+def basket_remove(request):
     try:
-        basket = Basket.objects.get(id=basket_id)
-        basket.delete()
-        return redirect(request.META["HTTP_REFERER"])
+        cart_id = request.POST.get("cart_id")
+        cart = Basket.objects.get(id=cart_id)
+        quanity = cart.quantity
+        cart.delete()
+
+        user_cart = get_user_baskets(request)
+        cart_items_html = render_to_string(
+        "baskets/includes/included_basket.html", {"carts": user_cart}, request=request)
+
+        response_data = {
+            "message": "Товар удален из корзины",
+            "cart_items_html": cart_items_html,
+            "quantity_deleted": quanity
+        }
+
+        return JsonResponse(response_data)
     
     except ObjectDoesNotExist:
         messages.warning(request=request, message=f"{request.user.username}, товар не найден!!!")
